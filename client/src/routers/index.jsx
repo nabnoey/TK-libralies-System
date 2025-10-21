eddf094659cd50b2b00137ffc232bf50b5b83c26
nabnoey <nabnoeyy@gmail.com>
2025-10-21 17:05:08 +0700

    update1

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Index from "../pages/index.jsx";
+
 import Home from "../pages/Home.jsx";
 import Movies from "../pages/Movies.jsx";
 import Karaoke from "../pages/Karaoke.jsx";
@@ -8,13 +8,10 @@ import UserProfile from "../pages/UserProfile.jsx";
 import Login from "../pages/Login.jsx";
 
 const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Index/>
-  },
+
 
   {
-    path:"/home",
+    path:"/",
     element:<Home/>
   },
 
